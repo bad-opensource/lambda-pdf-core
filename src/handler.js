b6768e6f9d0a973eabb1beaf7ef1e806edb25914
Franz <flr@dasburo.com>
2020-05-07 11:57:16 +0200

    chore(setup): setup

--- src/handler.js
+++ src/handler.js
@@ -1,4 +1,11 @@
-module.exports = ({version, templates = {}, helpers, mocks, schema}) => {
+module.exports = ({
+					  version,
+					  templates = {},
+					  helpers,
+					  mocks,
+					  schema,
+					  patchData
+				  }) => {
 	const atob = require('atob');
 	const {validate} = require('./lib/validate');
 	const {getCompiledTemplate} = require('./lib/handlebars')({helpers, partials: templates.partials});
@@ -65,9 +72,8 @@ module.exports = ({version, templates = {}, helpers, mocks, schema}) => {
 			return errorBody(JSON.stringify(errors));
 		}
 
-		// patchMediaUrlsWithDownsize(parsedData.data.media);
-
-		const html = getCompiledTemplate(templates.main, parsedData);
+		const patchedData = patchData ? patchData(parsedData) : parsedData;
+		const html = getCompiledTemplate(templates.main, patchedData);
 
 		return {
 			statusCode: 200,
@@ -78,72 +84,32 @@ module.exports = ({version, templates = {}, helpers, mocks, schema}) => {
 		};
 	};
 
+	module.pdf = async event => {
+		const pdf = require('./lib/pdf');
+
+		const parsedData = parseData(event);
+		if (!parsedData) {
+			return errorBody('No Data for parsedData');
+		}
+
+		const {valid, errors} = validate({schema, data: parsedData.data});
+		if (valid !== true) {
+			return errorBody(JSON.stringify(errors));
+		}
+
+		const patchedData = patchData ? patchData(parsedData) : parsedData;
+
+		const mainTemplate = getCompiledTemplate(templates.main, patchedData);
+		const footerTemplate = getCompiledTemplate(templates.footer, patchedData);
+		const headerTemplate = getCompiledTemplate(templates.header, patchedData);
+
+		const generatedPdfData = await pdf.getPdf(mainTemplate, headerTemplate, footerTemplate);
+
+		const fileName = ('filename' || '').replace(' ', '');
+		return pdf.getPdfResponse(generatedPdfData, fileName);
+	};
+
 	return module;
 };
-
-
-
-//
-// const patchMediaUrlsWithDownsize = media => {
-// 	if (!media) {
-// 		return {};
-// 	}
-// 	const mediaCopy = {...media};
-// 	Object.keys(mediaCopy).forEach(key => {
-// 		if (!!mediaCopy[key].url) {
-// 			mediaCopy[key].url = mediaCopy[key].url + '?downsize=500px:*'
-// 		}
-// 	});
-// 	return mediaCopy;
-// };
-//
-// /**
-//  * getPdf - pdf route handler
-//  * @param {event} event event
-//  * @returns {Promise<{body: string, statusCode: number}|{headers: {'Content-type': string, 'content-disposition': string}, isBase64Encoded: boolean, body: string, statusCode: number}>} pdf
-//  */
-// module.exports.getPdf = async event => {
-// 	console.time('getPdf');
-// 	const pdf = require('./lib/pdf');
-// 	console.time('getAudiCodeData');
-// 	const audiCodeData = getAudiCodeData(event);
-// 	if (!audiCodeData) {
-// 		return errorBody('No Data for audiCodeData');
-// 	}
-//
-//
-// 	console.timeEnd('getAudiCodeData');
-//
-// 	console.time('validate');
-// 	const {valid, errors} = validate({schema, data: audiCodeData.data});
-// 	if (valid !== true) {
-// 		return errorBody(JSON.stringify(errors));
-// 	}
-// 	console.timeEnd('validate');
-//
-// 	patchMediaUrlsWithDownsize(audiCodeData.data.media);
-//
-// 	console.time('compileTemplates');
-// 	const legacyPdfTemplate = getCompiledTemplate('legacy-pdf', audiCodeData);
-// 	const footerTemplate = getCompiledTemplate('partials/footer', audiCodeData);
-// 	const headerTemplate = getCompiledTemplate('partials/header', audiCodeData);
-// 	console.timeEnd('compileTemplates');
-//
-// 	console.time('generatedPdfData');
-// 	const generatedPdfData = await pdf.getPdf(legacyPdfTemplate, headerTemplate, footerTemplate);
-// 	console.timeEnd('generatedPdfData');
-//
-// 	const fileName = (audiCodeData.data.header.title || '').replace(' ', '');
-//
-// 	console.timeEnd('getPdf');
-// 	return pdf.getPdfResponse(generatedPdfData, fileName);
-// };
-//
-// /**
-//  * getHtml - html route handler
-//  * @param {event} event event
-//  * @returns {Promise<{headers: {'Content-Type': string}, body: (null|string|HandlebarsTemplateDelegate<any>), statusCode: number}|{body: string, statusCode: number}>} html
-//  */
-
 
 
